189e58dfb7dca1581083b59a31d37e509fb6dff1
linh4 <linh44h@gmail.com>
2019-04-07 00:41:31 -0400

    timeline

---
+++ timeline/index.js
@@ -0,0 +1,27 @@
+
+const items = document.querySelectorAll(".column");
+
+const isElementInViewport = (el) => {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+const renderCards = () => {
+  for (let i = 0; i < items.length; i++) {
+    if (isElementInViewport(items[i])) {
+      if(!items[i].classList.contains("in-view")){
+        items[i].classList.add("in-view");
+      }
+    } else if(items[i].classList.contains("in-view")) {
+        items[i].classList.remove("in-view");
+    }
+  }
+}
+
+window.addEventListener("load", renderCards);
+window.addEventListener("scroll", renderCards);
